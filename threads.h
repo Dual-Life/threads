ca951b5a58433cdeed9b1e615e515ff3790444ff
jdhedden <jdhedden@cpan.org>
2016-04-21 09:25:14 -0400

    threads v1.39

--- threads.h
+++ threads.h
@@ -8,5 +8,9 @@
 #ifndef SAVEBOOL
 #  define SAVEBOOL(a)
 #endif
+/* Devel::PPPort bug workaround - Signals are safe under 5.8.0 */
+#if ((PERL_REVISION == 5) && (PERL_VERSION == 8) && (PERL_SUBVERSION == 0))
+#  define PERL_SIGNALS_UNSAFE_FLAG 0
+#endif
 
 #endif
